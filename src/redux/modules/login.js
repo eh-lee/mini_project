8c07e8174d9282c16e4873d8449754b9c2986615
eh-lee <fornewstack@daum.net>
2023-06-08 00:17:48 +0900

    [Feat] login.js CSRF 공격 대응을 위한 fetchCsrfToken 함수 추가 및 적용 commit

--- src/redux/modules/login.js
+++ src/redux/modules/login.js
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { apis } from "../../shared/axios";
 import { cookies } from "../../shared/cookie";
+import { fetchCsrfToken } from "../../shared/fetchCsrfToken";
 
 // const initialState = [{}];
 
@@ -26,9 +27,13 @@ export const __signUp = createAsyncThunk("signUp", async (newUser, thunk) => {
 
 export const __login = createAsyncThunk("logIn", async (thisUser, thunk) => {
   try {
-    const response = await apis.post("api/login", thisUser);
+    const csrfToken = await fetchCsrfToken();
+    const response = await apis.post("api/login", thisUser, {
+      headers: {
+        "X-CSRF-Token": csrfToken,
+      },
+    });
     cookies.set("token", response.headers.authorization, { path: "/" });
-    // cookies.set("nickname", response.data.nickname, { path: "/" });
     return thunk.fulfillWithValue(thisUser);
   } catch (e) {
     console.log(e);
@@ -38,6 +43,7 @@ export const __login = createAsyncThunk("logIn", async (thisUser, thunk) => {
     return thunk.rejectWithValue(e);
   }
 });
+
 
 const initialState = {
   isLogin: false,
